d243a49a749e69bdedf34a9ac3dbb11b09aec283
Takemonist <gardenoftakemoto@gmail.com>
2021-04-20 03:07:52 +0900

    update

--- WheelsController.hpp
+++ WheelsController.hpp
@@ -1,4 +1,30 @@
 #ifndef IG_OMNIMECANUMCONTROLLER_HPP_
 #define IG_OMNIMECANUMCONTROLLER_HPP_
 
+enum struct MODEL
+{
+OMNI4_CORNER,
+OMNI4_SIDE,
+OMNI3,
+MECANUM
+};
+
+class WheelsController
+{
+private:
+    double *v1,*v2,*v3,*v4;
+    MODEL model;
+public:
+    WheelsController(MODEL model,double *var_1,double *var_2,double *var_3,double *var_4 = nullptr)
+    :v1(var_1),v2(var_2),v3(var_3),v4(var_4){};
+    void VelocityCalc(double Vx,double Vy,double w);
+    void VelocityCalc(double V,double q,double w);
+    ~WheelsController();
+};
+
+WheelsController::~WheelsController()
+{
+}
+
+
 #endif /* IG_OMNIMECANUMCONTROLLER_HPP_ */
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "WheelsController.hpp"
 
+double pe = 1;
 int main(){
+    std::cout << pe;
+WheelsController a(MODEL::OMNI4_CORNER,&pe,&pe,&pe);
 
-    std::cout << 2;
+    std::cout << pe;
 }
